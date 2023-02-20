7c3a3651d4b6766a537bdf1937670b00819ae0be
J-Pinos3 <josloco665@gmail.com>
2023-02-20 09:03:26 -0500

    Login_frm

---
+++ src/Vista/Login.java
@@ -0,0 +1,10 @@
+package Vista;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel panel_Login;
+    private JTextField txtCorreo;
+    private JPasswordField txtPass;
+    private JButton iniciarSesiónButton;
+}
