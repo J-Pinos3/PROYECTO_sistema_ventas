21af8f697f7c2a6147707bfe245416485779e30e
J-Pinos3 <josloco665@gmail.com>
2023-02-20 20:21:55 -0500

    Pestaña de Login(vista)

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -2,9 +2,19 @@ package Vista;
 
 import javax.swing.*;
 
-public class Login {
+public class Login extends JFrame{
     private JPanel panel_Login;
     private JTextField txtCorreo;
     private JPasswordField txtPass;
     private JButton iniciarSesiónButton;
+
+    public Login(){
+        setContentPane(panel_Login);
+        setVisible(true);
+        setTitle("Login");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(800,500);
+    }
+
+
 }
