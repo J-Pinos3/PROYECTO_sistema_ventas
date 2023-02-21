608404e87d4d49c5b21667d10829db830b8e41f3
J-Pinos3 <josloco665@gmail.com>
2023-02-21 08:47:12 -0500

    Pestaña del sistema principal(vista)

---
+++ src/Vista/Sistema.java
@@ -0,0 +1,25 @@
+package Vista;
+
+import javax.swing.*;
+
+public class Sistema {
+    private JPanel panel_Sistema;
+    private JTabbedPane tabbedPane1;
+    private JButton nuevaVentaButton;
+    private JButton productosButton;
+    private JButton clientesButton;
+    private JButton proveedoresButton;
+    private JButton ventasButton;
+    private JButton configButton;
+    private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTable table1;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JButton button2;
+    //la tabla tiene columnas CODIGO, DESCRIPCION, CANTIDAD, PRECIO, TOTAL
+}
