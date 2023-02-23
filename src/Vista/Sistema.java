6a8ef236f046fea96ddf316b7e5a6606e0a9af97
J-Pinos3 <josloco665@gmail.com>
2023-02-23 12:32:00 -0500

    Pestañas Proveedores Productos (vista)

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -21,5 +21,42 @@ public class Sistema {
     private JTextField textField6;
     private JTextField textField7;
     private JButton button2;
-    //la tabla tiene columnas CODIGO, DESCRIPCION, CANTIDAD, PRECIO, TOTAL
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTable table2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JTable table3;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JTextField textField18;
+    private JTextField textField19;
+    private JTextField textField20;
+    private JTextField textField21;
+    private JTable table4;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JComboBox comboBox1;
+    private JButton button15;
+    //EN EL PRIMER TAB. la tabla tiene columnas CODIGO, DESCRIPCION, CANTIDAD, PRECIO, TOTAL
+    //EN EL SEGUNDO TAB. la tabla tiene columnas Cédula/RUC, Nombre, Teléfono, Dirección, Razón Social
+    //EN EL TERCER TAB, la tabla tiene las columnas RUC, NOMBRE, TELÉFONO, DIRECCIÓN, RAZÓN SOCIAL
+    //En EL CUARTO TAB, la tabla tiene las columnas CÓDIGO, DESCRIPCIÓN, STROCK, PRECIO, PROVEEDOR
+
+
+    //video 4  5:35
 }
